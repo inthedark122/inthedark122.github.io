397e6ebe51e95055ddccb3b9aef9b5cac02d625d
Oleh Kutsenko <inthedark1225@gmail.com>
2020-02-25 22:58:15 +0200

    music portfolio v3

---
+++ music-portfolio-v3/precache-manifest.416f02af649f25b9be19808ee471fa4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d518e78de71cb22539277bcd289cf81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "365795fe7e74f1980cc7",
+    "url": "/static/js/2.e9c6279d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.e9c6279d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b170f9bd75023f4fe347",
+    "url": "/static/js/main.df621cbb.chunk.js"
+  },
+  {
+    "revision": "7f56427fc7575a23d79f",
+    "url": "/static/js/runtime-main.29988bec.js"
+  }
+]);
