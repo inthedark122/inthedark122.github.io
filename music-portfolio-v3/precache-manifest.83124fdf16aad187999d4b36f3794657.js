ae12abc508d28596051fff1cec2cd08cb0ce2fdf
Oleh Kutsenko <inthedark1225@gmail.com>
2020-02-25 23:04:20 +0200

    music portfolio add sw for v3

---
+++ music-portfolio-v3/precache-manifest.83124fdf16aad187999d4b36f3794657.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5741aca2a57a2e9dd25822bc8794bad",
+    "url": "/music-portfolio-v3/index.html"
+  },
+  {
+    "revision": "365795fe7e74f1980cc7",
+    "url": "/music-portfolio-v3/static/js/2.e9c6279d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/music-portfolio-v3/static/js/2.e9c6279d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b170f9bd75023f4fe347",
+    "url": "/music-portfolio-v3/static/js/main.df621cbb.chunk.js"
+  },
+  {
+    "revision": "2d2a5bcb3008635c2c41",
+    "url": "/music-portfolio-v3/static/js/runtime-main.8752b883.js"
+  }
+]);
